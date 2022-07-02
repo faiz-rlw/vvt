8bd74a8478ea56a5b78d2b20597d852fd9014e5c
yysr_rlw <2653662312@qq.com>
2022-07-02 22:25:17 +0800

    chore: remove evicePixelRatio

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-import DevicePixelRatio from "./utils/evicePixelRatio";
-onMounted(()=>{
-    DevicePixelRatio.init();
-})
-</script>
-
 <template>
     <router-view></router-view>
 </template>
