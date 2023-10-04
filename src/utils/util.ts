7ca961c4ff58d28ab980c2b1f128a4d124d05bf0
yysr_rlw <2653662312@qq.com>
2023-10-04 12:09:53 +0800

    feat: 1.新增防抖函数

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -34,3 +34,23 @@ export function confirm(title: string = "", msg: string = ""): Promise<any> {
 confirm.warn = (title: string = "", msg: string = "") => {
   return ElMessageBox.confirm(`${title}`, `${msg}`, { type: "warning" });
 };
+
+/**
+ * @description 防抖函数
+ * @param func 执行函数
+ * @param delay 延迟时间 
+ * @returns 
+ */
+export function debounce(func: Function, delay: number): Function {
+  let timer: ReturnType<typeof setTimeout> | null = null;
+  return function(this: any, ...args: any[]) {
+    // 如果有上一次的调用未执行，则取消操作
+    if (timer) {
+      clearTimeout(timer);
+    }
+    // 设置一个定时器，在延迟时间后执行操作
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
